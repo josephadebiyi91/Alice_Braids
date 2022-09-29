ac85ddcd0f44a9f3e7ebaaa36b6086b508e345d6
josephadebiyi91 <97675443+josephadebiyi91@users.noreply.github.com>
2022-09-29 06:40:30 -0700

    navbar correction 2

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// navbar toggle
+$(document).ready(function(){
+    $(".navbar-toggler").click(function(){
+     $(".navbar-collapse").show()
+    })
+    $(".nav-item").click(function(){
+      $(".navbar-collapse").hide()
+    })
+  })
+// end
